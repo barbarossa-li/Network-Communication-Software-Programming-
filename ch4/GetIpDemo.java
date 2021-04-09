1e239a1ad9c401670b4d975e6fc6f2dcb41d978f
barbarossa-li <75600640+barbarossa-li@users.noreply.github.com>
2021-04-09 14:18:48 +0800

    Update GetIpDemo.java

--- ch4/GetIpDemo.java
+++ ch4/GetIpDemo.java
@@ -104,5 +104,30 @@ public static void main(String[] args) {
 //writeContentToFile(content, "c.html");
 String content = getContentFromFile("c.html");
 System.out.println(getSpFromContent(content));
+  		File file = new File("C:/robots.txt");
+		FileInputStream fis = null;
+ 
+		try {
+			fis = new FileInputStream(file);
+ 
+			System.out.println("Total file size to read (in bytes) : "
+					+ fis.available());
+ 
+			int content;
+			while ((content = fis.read()) != -1) {
+				// convert to char and display it
+				System.out.print((char) content);
+			}
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fis != null)
+					fis.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
 }
 } 
